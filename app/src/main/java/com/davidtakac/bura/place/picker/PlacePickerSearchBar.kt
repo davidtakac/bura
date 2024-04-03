644a3a124687eb98640f0701f80984dfeb908516
davidtakac <david.takac@mailbox.org>
2024-04-03 16:10:17 +0200

    Copy internal search bar animation specs to make it edge-to-edge

--- app/src/main/java/com/davidtakac/bura/place/picker/PlacePickerSearchBar.kt
+++ app/src/main/java/com/davidtakac/bura/place/picker/PlacePickerSearchBar.kt
@@ -11,8 +11,15 @@
 package com.davidtakac.bura.place.picker
 
 import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.CubicBezierEasing
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,15 +28,63 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.davidtakac.bura.R
 import com.davidtakac.bura.place.Place
+
+internal object MotionTokens {
+    const val DurationExtraLong1 = 700.0
+    const val DurationExtraLong2 = 800.0
+    const val DurationExtraLong3 = 900.0
+    const val DurationExtraLong4 = 1000.0
+    const val DurationLong1 = 450.0
+    const val DurationLong2 = 500.0
+    const val DurationLong3 = 550.0
+    const val DurationLong4 = 600.0
+    const val DurationMedium1 = 250.0
+    const val DurationMedium2 = 300.0
+    const val DurationMedium3 = 350.0
+    const val DurationMedium4 = 400.0
+    const val DurationShort1 = 50.0
+    const val DurationShort2 = 100.0
+    const val DurationShort3 = 150.0
+    const val DurationShort4 = 200.0
+    val EasingEmphasizedCubicBezier = CubicBezierEasing(0.2f, 0.0f, 0.0f, 1.0f)
+    val EasingEmphasizedAccelerateCubicBezier = CubicBezierEasing(0.3f, 0.0f, 0.8f, 0.15f)
+    val EasingEmphasizedDecelerateCubicBezier = CubicBezierEasing(0.05f, 0.7f, 0.1f, 1.0f)
+    val EasingLegacyCubicBezier = CubicBezierEasing(0.4f, 0.0f, 0.2f, 1.0f)
+    val EasingLegacyAccelerateCubicBezier = CubicBezierEasing(0.4f, 0.0f, 1.0f, 1.0f)
+    val EasingLegacyDecelerateCubicBezier = CubicBezierEasing(0.0f, 0.0f, 0.2f, 1.0f)
+    val EasingLinearCubicBezier = CubicBezierEasing(0.0f, 0.0f, 1.0f, 1.0f)
+    val EasingStandardCubicBezier = CubicBezierEasing(0.2f, 0.0f, 0.0f, 1.0f)
+    val EasingStandardAccelerateCubicBezier = CubicBezierEasing(0.3f, 0.0f, 1.0f, 1.0f)
+    val EasingStandardDecelerateCubicBezier = CubicBezierEasing(0.0f, 0.0f, 0.0f, 1.0f)
+}
+
+private const val AnimationEnterDurationMillis: Int = MotionTokens.DurationLong4.toInt()
+private const val AnimationExitDurationMillis: Int = MotionTokens.DurationMedium3.toInt()
+private const val AnimationDelayMillis: Int = MotionTokens.DurationShort2.toInt()
+private val AnimationEnterEasing = MotionTokens.EasingEmphasizedDecelerateCubicBezier
+private val AnimationExitEasing = CubicBezierEasing(0.0f, 1.0f, 0.0f, 1.0f)
+
+private val AnimationEnterFloatSpec: FiniteAnimationSpec<Dp> = tween(
+    durationMillis = AnimationEnterDurationMillis,
+    delayMillis = AnimationDelayMillis,
+    easing = AnimationEnterEasing,
+)
+private val AnimationExitFloatSpec: FiniteAnimationSpec<Dp> = tween(
+    durationMillis = AnimationExitDurationMillis,
+    delayMillis = AnimationDelayMillis,
+    easing = AnimationExitEasing,
+)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -50,11 +105,16 @@ fun PlacePickerSearchBar(
         if (active) focusRequester.requestFocus()
         else focusRequester.freeFocus()
     }
+    val spacerWidth by animateDpAsState(
+        targetValue = if (active) 0.dp else 16.dp,
+        animationSpec = if (active) AnimationEnterFloatSpec else AnimationExitFloatSpec
+    )
 
-    Box(
+    Row(
         modifier = Modifier.fillMaxWidth(),
-        contentAlignment = Alignment.Center
+        //contentAlignment = Alignment.Center
     ) {
+        Spacer(modifier = Modifier.width(spacerWidth))
         SearchBar(
             query = query,
             onQueryChange = onQueryChange,
@@ -77,7 +137,9 @@ fun PlacePickerSearchBar(
             placeholder = {
                 Text(text = stringResource(id = R.string.place_picker_hint_search))
             },
-            modifier = Modifier.focusRequester(focusRequester)
+            modifier = Modifier
+                .weight(1f)
+                .focusRequester(focusRequester)
         ) {
             PlacePickerResults(
                 state = state,
@@ -85,6 +147,7 @@ fun PlacePickerSearchBar(
                 onPlaceDeleteClick = onPlaceDeleteClick
             )
         }
+        Spacer(modifier = Modifier.width(spacerWidth))
     }
 }
 
